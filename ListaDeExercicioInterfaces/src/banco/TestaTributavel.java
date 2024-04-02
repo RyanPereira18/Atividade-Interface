c07e929e3db3a1c391717121b1c4db6057553a6c
RyanPereira18 <103114440+RyanPereira18@users.noreply.github.com>
2024-04-01 22:13:31 -0300

    Add files via upload

---
+++ ListaDeExercicioInterfaces/src/banco/TestaTributavel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+
+public class TestaTributavel {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente();
+        
+        cc.deposita(100);
+        
+        System.out.println(cc.calculaTributos());
+        
+        //testando polimorfismo
+        
+        Tributavel t = cc;
+        System.out.println(t.calculaTributos());
+    }
+}
---
+++ ListaDeExercicioInterfaces/src/interfaces/Teste.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaces;
+
+
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AreaCalculavel a = new Retangulo(3, 2);
+        System.out.println("Area: " + a.calculaArea());
+        
+        AreaCalculavel b = new Circulo(6);
+        System.out.println("Area: " + b.calculaArea());
+        
+        AreaCalculavel c = new Quadrado(5);
+        System.out.println("Area: " + c.calculaArea());
+    }
+    
+}
